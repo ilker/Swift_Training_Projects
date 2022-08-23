5f0c2bdd1f2958e3544fe7a5c81ca18eda04b056
ilker tacyildiz <48527920+ilker@users.noreply.github.com>
2022-08-24 01:24:47 +0300

    Basic Instagram Clone + Firebase learning

---
+++ BasicInstagramClone/BasicInstagramClone/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  BasicInstagramClone
+//
+//  Created by ILKER on 21.08.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ BasicInstagramClone/BasicInstagramClone/UploadViewController.swift
@@ -0,0 +1,102 @@
+//
+//  UploadViewController.swift
+//  BasicInstagramClone
+//
+//  Created by ILKER on 21.08.2022.
+//
+
+import UIKit
+import Firebase
+import FirebaseStorage
+
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentText: UITextField!
+    @IBOutlet weak var uploadButtonControl: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    func makeAlert(titleText: String, messageText: String) {
+        let alert = UIAlertController(title: titleText, message: messageText, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
+    
+    @objc func chooseImage() {
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
+
+    @IBAction func uploadButton(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            imageReference.putData(data, metadata: nil) { metadata, error in
+                if error != nil {
+                    self.makeAlert(titleText: "Error", messageText: error?.localizedDescription ?? "Error")
+                } else {
+                    
+                    imageReference.downloadURL { url, error in
+                        
+                        if error == nil {
+                            let imageUrl = url?.absoluteString
+                            
+                            //DATABASE
+                            
+                            let firestoreDatabase = Firestore.firestore()
+                            
+                            var firestoreReference : DocumentReference?
+                            
+                            let firestorePost = ["imageUrl" : imageUrl!, "postedBy" : Auth.auth().currentUser!.email!, "postComment" : self.commentText.text!, "date" : FieldValue.serverTimestamp(), "likes" : 0] as [String : Any]
+                            
+                            firestoreReference = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { error in
+                                if error != nil {
+                                    self.makeAlert(titleText: "Error", messageText: error?.localizedDescription ?? "Error")
+                                } else {
+                                    self.imageView.image = UIImage(named: "upload.png")
+                                    self.commentText.text = ""
+                                    self.tabBarController?.selectedIndex = 0
+                                }
+                            })
+                            
+                        }
+                        
+                        
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    
+    
+    
+
+}
